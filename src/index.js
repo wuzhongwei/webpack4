820eb31c6a13fa44ba684c48a13bef39c2dad377
wuzhongwei <94050803@qq.com>
2019-03-27 19:03:07 +0800

    代码提交

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import react from 'react';
-import reactDom from 'react-dom';
+// import "@babel/polyfill";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-console.log(react,reactDom)
+class HelloMessage extends Component {
+  componentDidMount(){
+    this.add();
+  }
+  add(){
+    const a = new Set(['aa','bb']);
+    console.log(a)
+  }
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+// 加载组件到 DOM 元素 mountNode
+render(<HelloMessage name="John" />, document.getElementById('root'));
